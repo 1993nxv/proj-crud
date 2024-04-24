7ad23663e0318aa5a3eafbcc185b078df8be888d
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-04-24 20:58:19 -0300

    Criando component para cadastro de produto.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
 import { HomeComponent } from "./views/home/home.component";
 import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 const routes: Routes = [
   {
@@ -13,6 +15,10 @@ const routes: Routes = [
   {
     path: "produtos",
     component: ProductCrudComponent
+  },
+  {
+    path: "produtos/criar",
+    component: ProductCreateComponent
   }
 ];
 
