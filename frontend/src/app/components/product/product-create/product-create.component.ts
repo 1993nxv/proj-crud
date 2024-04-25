6470dd4f48d79a50fab9b74d4c408bcc963c1020
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-04-24 21:21:05 -0300

    Navegação pelo botao para a rota criar produto.

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -7,15 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit{
 
-  propLegal = "qualquer";
-
   constructor() { }
 
-  ngOnInit(): void {
-      
-  }
-
-  fazerAlgo(): void {
-    console.log('Fazendo algo!' + this.propLegal);
-  }
+  ngOnInit(): void { }
 }
