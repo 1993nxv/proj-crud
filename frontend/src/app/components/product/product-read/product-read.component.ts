c104c44d2c919f1d10beb087073ce729a5563d28
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-04-28 21:45:35 -0300

    Metodo excluir produto, com modal de confirmação.

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,6 +1,8 @@
 import { ProductService } from './../product.service';
 import { Component } from '@angular/core';
 import { Product } from '../product.model';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogConfirmationComponent } from '../../dialog-confirmation/dialog-confirmation.component';
 
 @Component({
   selector: 'app-product-read',
@@ -12,12 +14,32 @@ export class ProductReadComponent {
   products!: Product[];
   displayedColumns = ['id', 'nome', 'preco', 'action'];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.productService.read().subscribe( 
       products => {
       this.products = products;
       });
+  }
+
+  openDialog(id: string): void {
+    const dialogRef = this.dialog.open(DialogConfirmationComponent, {
+      data: 'Tem certeza que deseja remover o produto?',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      
+      if(result){
+        console.log('clicou sim')
+        this.productService.delete(id).subscribe(
+          product => {
+            this.productService.showMessaage('Produto excluido com sucesso!')
+          });
+      }
+    });
   }
 }
